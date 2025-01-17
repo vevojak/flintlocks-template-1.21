672a53565d6ffb81c8e69377ebabb124d9372573
vevojak <jackjackpost@gmail.com>
2025-01-17 18:51:35 -0500

    Ok this is actually everything

---
+++ src/main/java/net/jack/flintlocks/FlintlocksClient.java
@@ -0,0 +1,11 @@
+package net.jack.flintlocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlintlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
---
+++ src/main/java/net/jack/flintlocks/FlintlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jack.flintlocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlintlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
